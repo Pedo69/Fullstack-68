d9bba5ab2a436824f34ec6e0e4e1952ca9a8f1fa
Pedo69 <apicha.pm2549@gmail.com>
2026-01-06 14:10:41 +0700

    day4

---
+++ Lecture4/libarea.mjs
@@ -0,0 +1,13 @@
+function areaTriagle(b, h) {
+    return (b*h) / 2;
+}
+
+function areacicrele(r) {
+    return Math.PI * r * r;
+}
+
+function areaSqr(w, l) {
+    return w * l;
+}
+
+export {areaSqr, areaTriagle, areacicrele};
---
+++ Lecture4/usemod.js
@@ -0,0 +1,4 @@
+import { areacicrele,areaSqr } from "./libarea.mjs";
+
+console.log(areacicrele(7));
+console.log(areaSqr(5,6));
